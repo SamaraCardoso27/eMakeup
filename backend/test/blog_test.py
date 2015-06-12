4436f1f585539084713b9248774cecf9081d3953
SamaraCardoso27 <samaracardosodossantos@gmail.com>
2015-06-11 22:39:23 -0300

    Iniciando Testes

--- backend/test/blog_test.py
+++ backend/test/blog_test.py
@@ -7,24 +7,24 @@ from student.student_model import Course
 __author__ = 'samara'
 
 
-class NewTeste(GAETestCase):
-    def test_sucesso(self):
-        resposta = new.salvar(name='bla',)
-        self.assertIsInstance(resposta, RedirectResponse)
-        self.assertEqual('/courses',resposta.context)
-        cursos = Course.query().fetch()
-        self.assertEqual(1,len(cursos))
-        curso = cursos[0]
-        self.assertEqual('bla',curso.name)
+#class NewTeste(GAETestCase):
+#    def test_sucesso(self):
+#        resposta = new.salvar(name='bla',)
+#        self.assertIsInstance(resposta, RedirectResponse)
+#        self.assertEqual('/courses',resposta.context)
+#        cursos = Course.query().fetch()
+#        self.assertEqual(1,len(cursos))
+#        curso = cursos[0]
+#        self.assertEqual('bla',curso.name)
 
 
-    def test_validacao(self):
-        resposta = new.salvar()
-        self.assertIsInstance(resposta,TemplateResponse)
-        self.assert_can_render(resposta)
-        self.assertIsNone(Course.query().get())
-        self.maxDiff = True
-        self.assertDictEqual({u'course':{},u'erros':{'name':u'Required field'}},resposta.context)
+#    def test_validacao(self):
+#        resposta = new.salvar()
+#        self.assertIsInstance(resposta,TemplateResponse)
+#        self.assert_can_render(resposta)
+#        self.assertIsNone(Course.query().get())
+#        self.maxDiff = True
+#        self.assertDictEqual({u'course':{},u'erros':{'name':u'Required field'}},resposta.context)
 
 
 
